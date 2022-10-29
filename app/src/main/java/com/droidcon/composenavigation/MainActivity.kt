714d49e7a28c9eedaa462deefc455cef920aa457
Nicholas DiPatri <ndipatri@gmail.com>
2022-10-29 09:46:52 -0400

    no message

--- app/src/main/java/com/droidcon/composenavigation/MainActivity.kt
+++ app/src/main/java/com/droidcon/composenavigation/MainActivity.kt
@@ -1,22 +1,40 @@
 package com.droidcon.composenavigation
 
-import android.app.Activity
-import android.content.Context
 import android.os.Bundle
-import android.webkit.WebSettings
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.widget.FrameLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.C
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.rtsp.RtspMediaSource
+import androidx.media3.exoplayer.source.MediaSource
+import androidx.media3.exoplayer.util.EventLogger
+import androidx.media3.ui.AspectRatioFrameLayout
+import androidx.media3.ui.PlayerView
 import com.droidcon.composenavigation.ui.theme.ComposeNavigationTheme
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.*
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,8 +49,11 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Column {
-                        Button(onClick = { screenToShow = { StartSiren("") } }) {
-                            Text("Start Siren!")
+                        Button(onClick = { screenToShow = { ShowSiren() } }) {
+                            Text("Show Siren")
+                        }
+                        Button(onClick = { screenToShow = { StartSiren() } }) {
+                            Text("Start Siren")
                         }
 
                         screenToShow()
@@ -50,10 +71,110 @@ fun Greeting(name: String) {
 
 @Composable
 fun ShowSiren() {
-    Text(text = "Here is our siren!")
+    Text(text = "This is the 'Show Siren' Screen")
+
+    LiveRedSirenVideoPlayer()
 }
 
 @Composable
 fun StartSiren() {
-    Text(text = "Starting the Siren...")
+    Text(text = "This is the 'Start Siren' Screen")
+
+    LiveRedSirenVideoPlayer()
+
+    LaunchedEffect(Unit ) {
+        withContext(Dispatchers.IO) {
+            particleInterface.turnOnRedSiren().execute()
+            delay(3000)
+            particleInterface.turnOffRedSiren().execute()
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+val particleInterface: ParticleRESTInterface by lazy {
+
+    val okHttpClient = OkHttpClient()
+
+    Retrofit.Builder().apply {
+        baseUrl("https://api.particle.io/")
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+    }
+        .build()
+        .create(ParticleRESTInterface::class.java)
+}
+
+
+interface ParticleRESTInterface {
+    @GET("/v1/devices?access_token=$particleToken")
+    suspend fun getDevices(): List<SimpleDevice>
+
+    @FormUrlEncoded
+    @POST("/v1/devices/{deviceId}/sirenOn")
+    fun turnOnRedSiren(
+        @Path("deviceId") deviceId: String = "e00fce6829afbb59f9e1a1fb",
+        @Field("arg") updateState: String = "",
+        @Field("access_token") accessToken: String = particleToken
+    ): Call<Unit>
+
+    @FormUrlEncoded
+    @POST("/v1/devices/{deviceId}/sirenOff")
+    fun turnOffRedSiren(
+        @Path("deviceId") deviceId: String = "e00fce6829afbb59f9e1a1fb",
+        @Field("arg") updateState: String = "",
+        @Field("access_token") accessToken: String = particleToken
+    ): Call<Unit>
+}
+
+const val particleToken = "78116e05f59f44d8142c22a86216d8103aa7bdec"
+
+
+@Composable
+@androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
+fun LiveRedSirenVideoPlayer() {
+    val context = LocalContext.current
+
+    val exoPlayer = remember {
+        ExoPlayer.Builder(context)
+            .build()
+            .apply {
+
+                val rtspMediaSource: MediaSource = RtspMediaSource.Factory()
+                    .createMediaSource(MediaItem.fromUri("rtsp://ndipatri:ndipatri@10.0.0.21:554/stream1"))
+
+                setMediaSource(rtspMediaSource)
+                addAnalyticsListener(EventLogger(null, "exo"))
+                prepare()
+            }
+    }
+
+    exoPlayer.playWhenReady = true
+    exoPlayer.videoScalingMode = C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING
+    exoPlayer.repeatMode = Player.REPEAT_MODE_ONE
+
+    DisposableEffect(
+        AndroidView(factory = {
+            PlayerView(context).apply {
+                hideController()
+                useController = false
+                resizeMode = AspectRatioFrameLayout.RESIZE_MODE_ZOOM
+
+                player = exoPlayer
+                layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+            }
+        })
+    ) {
+        onDispose { exoPlayer.release() }
+    }
 }
