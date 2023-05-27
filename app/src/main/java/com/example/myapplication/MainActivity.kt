c6132d5819ca4fc285e3e2ee0192787807c5bfd6
Nicholas DiPatri <ndipatri@gmail.com>
2023-05-27 06:57:50 -0400

    welcome screen.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -62,6 +62,34 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.*
 import java.util.*
 
+/*
+
+
+
+
+  _____                          _     _                  _      _____
+ |  __ \                        | |   (_)                | |    / ____|
+ | |__) |  _ __    __ _    ___  | |_   _    ___    __ _  | |   | |        ___    _ __ ___    _ __     ___    ___    ___
+ |  ___/  | '__|  / _` |  / __| | __| | |  / __|  / _` | | |   | |       / _ \  | '_ ` _ \  | '_ \   / _ \  / __|  / _ \
+ | |      | |    | (_| | | (__  | |_  | | | (__  | (_| | | |   | |____  | (_) | | | | | | | | |_) | | (_) | \__ \ |  __/
+ |_|      |_|     \__,_|  \___|  \__| |_|  \___|  \__,_| |_|    \_____|  \___/  |_| |_| |_| | .__/   \___/  |___/  \___|
+                                                                                            | |
+                                                                                            |_|
+  _   _                   _                   _     _
+ | \ | |                 (_)                 | |   (_)
+ |  \| |   __ _  __   __  _    __ _    __ _  | |_   _    ___    _ __
+ | . ` |  / _` | \ \ / / | |  / _` |  / _` | | __| | |  / _ \  | '_ \
+ | |\  | | (_| |  \ V /  | | | (_| | | (_| | | |_  | | | (_) | | | | |
+ |_| \_|  \__,_|   \_/   |_|  \__, |  \__,_|  \__| |_|  \___/  |_| |_|
+                               __/ |
+                              |___/
+DroidCon San Francisco 2023
+Nick DiPatri
+https://www.linkedin.com/in/ndipatri/
+https://github.com/ndipatri/composeNavigation/tree/finish
+*/
+
+
 class PureComposeBridgeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
